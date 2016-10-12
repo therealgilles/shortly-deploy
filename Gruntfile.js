b540eb961ed230ae6b8a8e0d274c5d64134a7d47
Hack Reactor Students <accounts@hackreactor.com>
2016-10-12 15:31:39 -0700

    A few changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,18 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    clean: [ 'public/dist/' ],
+
     concat: {
+      client: {
+        files: {
+          'public/dist/build.js': [
+            'public/client/**/*.js',
+            'public/lib/**/*.js'
+          ]
+        },
+      }
     },
 
     mochaTest: {
@@ -21,15 +32,46 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      dynamic_mappings: {
+        // Grunt will search for "**/*.js" under "lib/" when the "uglify" task
+        // runs and build the appropriate src-dest file mappings then, so you
+        // don't need to update the Gruntfile when files are added or removed.
+        files: [
+          {
+            expand: true,          // Enable dynamic expansion.
+            cwd: 'public/build/',  // Src matches are relative to this path.
+            src: ['**/*.js'],      // Actual pattern(s) to match.
+            dest: 'public/build/', // Destination path prefix.
+            ext: '.min.js',        // Dest filepaths will have this extension.
+            extDot: 'first'        // Extensions in filenames begin after the first dot
+          },
+        ],
+      },
     },
 
     eslint: {
       target: [
-        // Add list of files to lint here
+        'server.js',
+        'server-config.js',
+        'app/*.js',
+        'lib/*.js',
+        'views/**/*.js',
+        'public/client/**/*.js',
+        'public/lib/**/*.js',
+        'test/*.js'
       ]
     },
 
     cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/dist/',
+          ext: '.min.css'
+        }]
+      }
     },
 
     watch: {
@@ -59,6 +101,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
@@ -77,6 +120,7 @@ module.exports = function(grunt) {
   ]);
 
   grunt.registerTask('build', [
+    'clean', 'eslint', 'test'
   ]);
 
   grunt.registerTask('upload', function(n) {
@@ -88,8 +132,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('deploy', [
-    // add your deploy tasks here
+    'build', 'concat', 'uglify', 'cssmin'
   ]);
-
 
 };
